b208f0f32586bb973106e0fda4d0c7901aa529d4
minidonut <nycom13@gmail.com>
2020-09-25 23:13:03 +0900

    implement peco binding

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("hello node-peco");
+import { spawn } from 'child_process';
+
+const peco = async (data: string, prompt = 'find'): Promise<string> => {
+  return new Promise(resolve => {
+    const peco = spawn('peco', ['--layout=bottom-up', `--prompt=[${prompt}]`]);
+
+    let selected = '';
+    peco.stdout.on('data', data => {
+      selected += data;
+    });
+
+    peco.stdout.on('end', () => {
+      resolve(selected.trim());
+    });
+
+    peco.stdin.write(data);
+    peco.stdin.end();
+  });
+};
+
+// export default peco;
