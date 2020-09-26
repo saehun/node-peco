eea2ff979baf0f2ed8a267dfcea49aaee304227d
minidonut <nycom13@gmail.com>
2020-09-27 00:14:10 +0900

    test reject with example1

--- examples/example1.ts
+++ examples/example1.ts
@@ -3,6 +3,6 @@ import peco from '../src';
 const data = ['option1', 'option2', 'option3'].join('\n');
 
 (async (): Promise<void> => {
-  const selected = await peco(data);
+  const selected = await peco(data, { reject: false });
   console.log(selected);
 })();
