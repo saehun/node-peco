65e5c4c34ad7faaf557fc99ed4a9456d471b3d63
minidonut <nycom13@gmail.com>
2020-09-27 16:50:33 +0900

    add option

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,38 @@
 import { spawn } from 'child_process';
 
-const peco = async (data: string, prompt = 'find'): Promise<string> => {
-  return new Promise(resolve => {
-    const peco = spawn('peco', ['--layout=bottom-up', `--prompt=[${prompt}]`]);
+const handleError = (option: Option, reject: (reason?: any) => void) => (error: Error): void => {
+  if (option.reject) {
+    reject(error);
+  } else {
+    if (error.message.includes('ENOENT')) {
+      console.error(`command not found: peco\ninstall peco from https://github.com/peco/peco`);
+    } else {
+      console.error(error);
+    }
+    process.exit(1);
+  }
+};
+
+interface Option {
+  path?: string;
+  reject?: boolean;
+  query?: string;
+  prompt?: string;
+  selectOne?: boolean;
+  printQuery?: boolean;
+  initialIndex?: number;
+  initialFilter?: 'IgnoreCase' | 'CaseSensitive' | 'SmartCase' | 'Regexp' | 'Fuzzy';
+  onCancel?: 'success' | 'error';
+  layout?: 'bottom-up' | 'top-down';
+}
+
+const getPath = (option: Option): string => {
+  return option.path ?? 'peco';
+};
+
+const peco = async (data: string, option: Option): Promise<string[]> => {
+  return new Promise((resolve, reject) => {
+    const peco = spawn('peco', ['--layout=bottom-up', `--prompt=[find]`]);
 
     let selected = '';
     peco.stdout.on('data', data => {
@@ -10,8 +40,11 @@ const peco = async (data: string, prompt = 'find'): Promise<string> => {
     });
 
     peco.stdout.on('end', () => {
-      resolve(selected.trim());
+      resolve(selected.trim().split('\n'));
     });
+
+    peco.on('error', handleError(option, reject));
+    peco.stdin.on('error', handleError(option, reject));
 
     peco.stdin.write(data);
     peco.stdin.end();
